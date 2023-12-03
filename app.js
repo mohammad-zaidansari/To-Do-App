cf9717de7d340f45ad670615dca59ccdef553534
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2023-12-04 00:17:48 +0530

    add comments

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const listContainer = document.getElementById("list-container");
 
 function addTask(){
     if(inputBox.value === ''){
-        alert("you must write something!")
+        alert("you must write something!");
     }else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
 
         let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+        span.innerHTML = "\u00d7";  //Cross icon added
+        li.appendChild(span);    
     }
     inputBox.value = "";
     saveData();
@@ -19,10 +19,10 @@ function addTask(){
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
+        e.target.classList.toggle("checked"); // check the task 
         saveData();
     }else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
+        e.target.parentElement.remove();  // remove the task 
         saveData();
     }
 }, false);
